73a48a98d2e4a1d579dd9c7d107114123d2330e0
smallC233 <253944741@qq.com>
2024-01-11 12:19:16 +0800

    完成了部分 SSDClient 和 SSDServer

--- SSDClient.py
+++ SSDClient.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import socket
+import pickle
+import os
+import sys
+import shutil
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def send_request():
+    host = "127.0.0.1"
+    port = 5555
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            client_socket.connect((host, port))
+            break
+        except ConnectionRefusedError:
+            s = input("连接失败，服务端未开启，输入任意键重试...")
+            if s == "exit":
+                return False
+
+    p = input("""请选择操作：
+1. 新建文件
+2. 删除文件
+3. 复制文件至硬盘
+4. 复制硬盘文件至指定文件夹
+5. 查看硬盘使用情况
+6. 退出
+7. 关闭服务端并退出
+> """)
+    if p == "1":
+        name = input("请输入新建文件的文件名：\n> ")
+        size = input("请输入新建文件的大小：\n> ")
+
+        client_socket.send(pickle.dumps([1, name, size]))
+
+    elif p == "2":
+        address = int(input("请输入删除的起始地址：\n> "))
+        client_socket.send(pickle.dumps([2, address]))
+
+    elif p == "3":
+        fp = input("请输入被拷贝文件的路径：\n> ")
+        if fp[0] == "\"" and fp[-1] == "\"":
+            fp = fp[1:-1]
+
+        fp = os.path.abspath(fp)
+        client_socket.send(pickle.dumps([3, fp]))
+
+    elif p == "4":
+        client_socket.send(pickle.dumps([4]))
+
+    elif p == "5":
+        client_socket.send(pickle.dumps([5]))
+
+    elif p == "6":
+        client_socket.close()
+        return False
+
+    elif p == "7":
+        client_socket.send(pickle.dumps([7]))
+        response = client_socket.recv(1024)
+        response = pickle.loads(response)
+        print(response)
+        client_socket.close()
+        return False
+
+    else:
+        client_socket.send(pickle.dumps("未知指令"))
+
+    # 接收服务器的响应
+    response = client_socket.recv(1024)
+
+    response = pickle.loads(response)
+
+    print(response)
+
+    client_socket.close()
+
+    return True
+
+
+if __name__ == "__main__":
+    # sys.stdin = open('in.txt', 'r')
+    while send_request():
+        pass
--- SSDServer.py
+++ SSDServer.py
@@ -1,16 +1,107 @@
-# This is a sample Python script.
+import socket
+import pickle
+from threading import Thread
+import os
+import time
+from Modulo.SSD import SSD
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+client_handler_queue = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def copy_in(fp):
+    address = ssd.create(os.stat(fp).st_size)
+    ssd.copy_in(fp, address)
+    return "success"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def work(li, client_socket):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    result = "Unknown Error"
+    if li[0] == 1:
+        address = ssd.create(li[2])
+        Dict[address] = li[1]
+        result = "创建成功"
+
+    elif li[0] == 2:
+        try:
+            ssd.delete(li[1])
+            result = "success"
+        except Exception as e:
+            result = "Error: " + str(e)
+
+    elif li[0] == 3:
+        try:
+            result = copy_in(li[1])
+        except Exception as e:
+            result = "Error: " + str(e)
+
+    elif li[0] == 4:
+        pass
+
+    elif li[0] == 5:
+        result = ssd.list()
+
+    elif li[0] == 7:
+        client_socket.send(pickle.dumps("已发出关闭指令"))
+        client_socket.close()
+        return
+
+    else:
+        result = "未知指令"
+
+    # 发送结果给客户端
+    client_socket.send(pickle.dumps(result))
+    client_socket.close()
+    return
+
+
+def start_server():
+    host = "127.0.0.1"
+    port = 5555
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+
+    # 最多监听十个连接。
+    server_socket.listen(10)
+
+    print(f"Server listening on {host}:{port}")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Accepted connection from {client_address}")
+
+        li = client_socket.recv(1024)
+        if not li:
+            continue
+
+        li = pickle.loads(li)
+
+        client_handler = Thread(target=work, args=(li, client_socket))
+        client_handler_queue.append(client_handler)
+        client_handler.start()
+
+        if li[0] == 7:
+            for client_handler in client_handler_queue:
+                client_handler.join()
+
+            return
+
+
+if __name__ == "__main__":
+    path = os.path.abspath("data")
+    # print(path)
+
+    if os.path.exists(os.path.join(path, "dict.bin")):
+        with open(path + "\\dict.bin", "rb") as file:
+            Dict = pickle.loads(file.read())
+            print(Dict)
+    else:
+        Dict = {}
+
+    ssd = SSD("E:/VirtualSSD", (64 << 30), 8, (4 << 10))
+    start_server()
+    ssd.close()
+
+    with open(path + "\\dict.bin", "wb") as file:
+        file.write(pickle.dumps(Dict))
