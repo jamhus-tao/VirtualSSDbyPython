48a72691394c657519b77c1d71fc66b4b66329ad
jamhus-tao <jamhus_tao@qq.com>
2024-01-12 10:25:24 +0800

    现在 mapping fix 也与 Server.yml 配置绑定, 同时清理 dict.bin

--- mapping_fix.py
+++ mapping_fix.py
@@ -1,7 +1,28 @@
+import os
+import yaml
 from Modulo.SSD import SSD
 
+
+class Configer:
+    def __init__(self, fp: str = "Server.yml"):
+        self.fp = fp
+        if not os.path.exists(fp):
+            raise AttributeError("Config YAML file not fount: {}".format(self.fp))
+        with open(fp, "r") as _file:
+            _data = yaml.safe_load(_file)
+        try:
+            self.PATH = _data["ssd"]
+            self.HOST = _data["host"]
+            self.PORT = _data["port"]
+            self.DICT = _data.get("dict", "dict.bin")
+        except KeyError:
+            raise AttributeError("Necessary config missing")
+
+
 if __name__ == "__main__":
-    ssd = SSD("E:/", (64 << 30), 8, (4 << 10))
+    cfg = Configer()
+    ssd = SSD(cfg.PATH, (64 << 30), 8, (4 << 10))
     ssd._mapping._init_mapping()
     ssd._mapping._load_mapping()
+    os.remove(os.path.join(cfg.PATH, cfg.DICT))
     ssd.close()
